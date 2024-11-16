ee557df0078cd282cf1a705deb75ff1bcfa5f134
mfsolars <mftrading901@gmail.com>
2024-11-16 23:29:01 +0500

    Updating Token Response

--- src/utils/responseWithToken.js
+++ src/utils/responseWithToken.js
@@ -2,9 +2,10 @@ const { generateTokenFromid } = require('./generateToken');
 
 const sendTokenResponse = (User_current, statusCode, message, res) => {
     const options = {
-        withCredentials: true
-        // httpOnly: false
-    }
+    httpOnly: true, // Recommended for security
+    sameSite: 'None', // Ensure this is a string and case-sensitive
+    secure: true, // This ensures the cookie is only sent over HTTPS
+};
     const token_for_user = generateTokenFromid(User_current._id);
     res.cookie('token', token_for_user, options);
     res.status(statusCode).json({
