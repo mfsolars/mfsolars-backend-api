4669957522c3e8713b26f0bb881923922ef7732b
Muhammad Wajahat <141699744+RMWajahat@users.noreply.github.com>
2024-11-16 21:40:35 +0500

    testing origins

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(cookieParser()); // cookie parser ka use kara ga
 
 // cors usage
 app.use(crossOriginSharing({
-    origin: ["https://mfsolars-frontend-production.up.railway.app","HTTP://localhost:4000"],  
+    origin: ["https://mfsolars-frontend-production.up.railway.app","http://localhost:5173","https://www.mfsolars.com"],  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
